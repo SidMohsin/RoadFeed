799f3a42edb4e926f0f2a02266bd51943067adcb
Mohsin <mohsinsiddique07860@gmail.com>
2024-09-25 11:56:57 +0530

    Upated

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const FeedbackForm = () => {
   const [formData, setFormData] = useState({
     name: '',
-    email: '',
+    email: '',  
     feedback: '',
     image: null,
   });
