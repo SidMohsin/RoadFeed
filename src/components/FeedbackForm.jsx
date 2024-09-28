dbd77e8beeb1eadbdad560ced151e556aea4d8e9
Junaid <me.zunaidkhan@gmail.com>
2024-09-28 16:31:25 +0530

    Updated Feedback form css

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -72,7 +72,7 @@ const FeedbackForm = () => {
           'Content-Type': 'multipart/form-data',
         },
       });
-      if(res.data.code<=210){
+      if (res.data.code <= 210) {
         console.log("Form Submitted");
         setFormData({
           Name: "",
@@ -88,7 +88,7 @@ const FeedbackForm = () => {
         setImagePreview(null)
         alert('Form Submitted')
 
-      }else{
+      } else {
         console.log('Error in submitting form');
       }
     } catch (error) {
@@ -100,115 +100,125 @@ const FeedbackForm = () => {
       <MapModel />
       <h2 className="form-title">Submit Feedback on Road Conditions</h2>
       <form className="form" onSubmit={handleSubmit}>
-        {/* Name Field */}
-        <div className="Input-Container">
-          <label className="label" htmlFor="Name">Name</label>
-          <input
-            type="text"
-            name="Name"
-            value={formData.Name}
-            onChange={handleInputChange}
-            placeholder="Your Name"
-            required
-            className="input"
-          />
+        <div className='d-md-flex flex-col justify-content-between gap-3'>
+          {/* Name Field */}
+          <div className="Input-Container">
+            <label className="label" htmlFor="Name">Name</label>
+            <input
+              type="text"
+              name="Name"
+              value={formData.Name}
+              onChange={handleInputChange}
+              placeholder="Your Name"
+              required
+              className="input"
+            />
+          </div>
+
+          {/* Email Field */}
+          <div className="Input-Container">
+            <label className="label" htmlFor="Email">Email</label>
+            <input
+              type="email"
+              name="Email"
+              value={formData.Email}
+              onChange={handleInputChange}
+              placeholder="Your Email"
+              required
+              className="input"
+            />
+          </div>
+        </div>
+        <div className='d-md-flex flex-col justify-content-between gap-3'>
+
+          {/* Number Field */}
+          <div className="Input-Container">
+            <label className="label" htmlFor="Number">Number</label>
+            <input
+              type="number"
+              name="Number"
+              value={formData.Number}
+              onChange={handleInputChange}
+              placeholder="Your Mobile No :"
+              required
+              className="input"
+            />
+          </div>
+
+          {/* Category Field */}
+          <div className="Input-Container">
+            <label className="label" htmlFor="Category">Category</label>
+            <select name="Category" id="category" className="select" defaultValue={'Potholes'} onChange={handleInputChange}>
+              <option value="Pothole" >Pothole</option>
+              <option value="Cracks">Cracks</option>
+              <option value="Blocked-road">Blocked Road</option>
+              <option value="Flooding">Flooding</option>
+              <option value="Traffic-signs">Missing/Damaged Traffic Signs</option>
+              <option value="Street-lights">Street Lights Issue</option>
+            </select>
+          </div>
+        </div>
+        <div className="d-md-flex flex-col justify-content-between gap-3">
+          <div className='Input-Container'>
+            <label className="label">Latitude</label>
+            <input type="number" name="Latitude" value={Marker.lat} className="input" readOnly />
+          </div>
+          <div className='Input-Container'>
+
+            <label className="label">Longitude</label>
+            <input type="number" name="Longitude" value={Marker.lng} className="input" readOnly />
+          </div>
+
+        </div>
+        <div className='d-md-flex flex-col justify-content-between gap-3'>
+
+          <div className="Input-Container">
+            <label className="label" htmlFor="Severity" >Severity</label>
+            <select name="Saverity" id="Saverity" className="select" defaultValue={"Low"} onChange={handleInputChange}>
+              <option value="High">High</option>
+              <option value="Medium">Medium</option>
+              <option value="Low">Low</option>
+            </select>
+          </div>
+
+          {/* Description Field */}
+          <div className="Input-Container">
+            <label className="label" htmlFor="Description">Description</label>
+            <textarea
+              name="Description"
+              value={formData.Description}
+              onChange={handleInputChange}
+              placeholder="Describe the issue..."
+              className="textarea"
+              cols={10}
+            />
+          </div>
         </div>
 
-        {/* Email Field */}
-        <div className="Input-Container">
-          <label className="label" htmlFor="Email">Email</label>
-          <input
-            type="email"
-            name="Email"
-            value={formData.Email}
-            onChange={handleInputChange}
-            placeholder="Your Email"
-            required
-            className="input"
-          />
-        </div>
 
-        {/* Number Field */}
-        <div className="Input-Container">
-          <label className="label" htmlFor="Number">Number</label>
-          <input
-            type="number"
-            name="Number"
-            value={formData.Number}
-            onChange={handleInputChange}
-            placeholder="Your Mobile No :"
-            required
-            className="input"
-          />
-        </div>
-
-        {/* Category Field */}
-        <div className="Input-Container">
-          <label className="label" htmlFor="Category">Category</label>
-          <select name="Category" id="category" className="select" defaultValue={'Potholes'} onChange={handleInputChange}>
-            <option value="Pothole" >Pothole</option>
-            <option value="Cracks">Cracks</option>
-            <option value="Blocked-road">Blocked Road</option>
-            <option value="Flooding">Flooding</option>
-            <option value="Traffic-signs">Missing/Damaged Traffic Signs</option>
-            <option value="Street-lights">Street Lights Issue</option>
-          </select>
-        </div>
-        <div className="Input-Container">
-          <label className="label" htmlFor="Severity" >Severity</label>
-          <select name="Saverity" id="Saverity" className="select" defaultValue={"Low"} onChange={handleInputChange}>
-            <option value="High">High</option>
-            <option value="Medium">Medium</option>
-            <option value="Low">Low</option>
-          </select>
-        </div>
-
-        {/* Description Field */}
-        <div className="Input-Container">
-          <label className="label" htmlFor="Description">Description</label>
-          <textarea
-            name="Description"
-            value={formData.Description}
-            onChange={handleInputChange}
-            placeholder="Describe the issue..."
-            className="textarea"
-            cols={10}
-          />
-        </div>
 
         {/* Image Upload Field */}
-        <div className="Input-Container-Image">
-          <label className="label">Upload Image</label>
-          <input type="file" name="Image" onChange={handleImageChange} className="input" required />
+        <div className="Input-Container-Image gap-3">
+          <div className='Input-image'>
+            <label className="label">Upload Image</label>
+            <input type="file" name="Image" onChange={handleImageChange} className="input" required />
+
+          </div>
 
           {/* Image Preview */}
           {imagePreview && (
             <div className="image-preview">
-              <img src={imagePreview} alt="Selected" width="200px" height="auto" />
+              <img src={imagePreview} alt="Selected" className='Image-preview-img' />
             </div>
           )}
         </div>
-        <div className="Input-Container-map">
-          <div className='Input-Container-latLng'>
-            <div className='Input-Container'>
-              <label className="label">Latitude</label>
-              <input type="number" name="Latitude" value={Marker.lat} className="input" readOnly />
-            </div>
-            <div className='Input-Container'>
+        <div className='d-flex gap-3'>
 
-              <label className="label">Longitude</label>
-              <input type="number" name="Longitude" value={Marker.lng} className="input" readOnly />
-            </div>
-            <div>
-
-              <button type="button" className="btn btn-toggle-map" onClick={() => { handleShow(); HandleLocationClick(); }}>
-                Location
-              </button>
-            </div>
-          </div>
+          <button type="button" className="btn btn-toggle-map submit-button" onClick={() => { handleShow(); HandleLocationClick(); }}>
+            Change Location
+          </button>
+          <button type="submit" className="submit-button">Submit Report</button>
         </div>
-
-        <button type="submit" className="submit-button">Submit Report</button>
       </form>
     </div>
   );
